8e0a79d01d2304a1bfedbd51464b961c86f7addd
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-10-26 23:44:56 +0200

    Create Task.h

---
+++ Task.h
@@ -0,0 +1,17 @@
+/*
+ * Task.h
+ *
+ *  Created on: Oct 26, 2019
+ *      Author: Rafaat
+ */
+
+#ifndef KEYPAD_LCD_MOAWAD_RAAFAT_TASK_H_
+#define KEYPAD_LCD_MOAWAD_RAAFAT_TASK_H_
+
+#include<avr/io.h>
+
+void Task_2m(void);
+void System_Init(void);
+void Keypad_100(void);
+void Question_task_10_sec (void);
+#endif /* KEYPAD_LCD_MOAWAD_RAAFAT_TASK_H_ */
